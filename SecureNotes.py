97df92dfdbe4256e7e03d28c1ee73579e3910202
Zac Nolan <zac1i1nolan@gmail.com>
2021-02-13 12:44:36 +0000

    Add files via upload

--- SecureNotes.py
+++ SecureNotes.py
@@ -36,16 +36,30 @@ ul.place(x = 50, y = 180, anchor = 'center')
 ul = Label(text = "Password:", bg = "pale turquoise", fg = "SteelBlue4")
 ul.place(x = 50, y = 200, anchor = 'center')
 
+#Checking Login Details
+def login(user, passwor):
+    if user=="Ma1war3" or user=="Lem0naise":
+        if passwor== "password":
+            congrats = Label(text="Congratulations, you have successfully logged in!", bg = "pale turquoise")
+            congrats.place(x=300, y=250, anchor= "center")
+
+#you need to keep the test here, it doesn't work without it but i don't know why
+def startlogin(test):
+    login(username.get(), password.get())
+
+#binding the enter key to the enter button 
+root.bind('<Return>', startlogin)
+
 #Input Bar
-message = StringVar()
+username = StringVar()
 mess = Entry(textvariable = username, width = 60, borderwidth=0)
 mess.place(x= 300, y = 180, anchor = 'center')
 
-message = StringVar()
-mess = Entry(textvariable = password, width = 60, borderwidth=0)
-mess.place(x= 300, y = 200, anchor = 'center')
+password = StringVar()
+mess2 = Entry(textvariable = password, width = 60, borderwidth=0)
+mess2.place(x= 300, y = 200, anchor = 'center')
 
-#Checking Login Details
+
 
 
 root.mainloop()
