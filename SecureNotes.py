77d0596141f33a364d8077f98ba8ad57a7c3baed
Zac Nolan <zac1i1nolan@gmail.com>
2021-02-14 22:12:21 +0000

    HUGE update, added one settings checkbox

--- SecureNotes.py
+++ SecureNotes.py
@@ -16,6 +16,11 @@ isempty = False
 password = StringVar()
 username = StringVar()
 logins = {}
+global autosave
+global darkmode
+darkmode = "True"
+global PaschaHuevo
+PaschaHuevo = ''
 
 #creating font
 deffont = font.Font(family = "arial", size="1")
@@ -38,6 +43,18 @@ signupbutton = Image.open("assets\sign_up.png")
 signupbutton = signupbutton.resize((131, 25), Image.ANTIALIAS)
 subutton = ImageTk.PhotoImage(signupbutton)
 
+backimage = Image.open("assets\\settings_icon.png")
+backimage = backimage.resize((25,25), Image.ANTIALIAS)
+settings_icon = ImageTk.PhotoImage(backimage)
+
+backimage = Image.open("assets\\box_checked.png")
+backimage = backimage.resize((36,33), Image.ANTIALIAS)
+boxchecked = ImageTk.PhotoImage(backimage)
+
+backimage = Image.open("assets\\box_unchecked.png")
+backimage = backimage.resize((36,33), Image.ANTIALIAS)
+boxunchecked = ImageTk.PhotoImage(backimage)
+
 #password decryption table
 decrypted = b"abcdefghijklmnop!qrstuvwxyz1234567_890ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 encrypted = b"zcxBVMNlkjhgFASDqEwT+RUyoIPZCXbvmnLKJHGfas!dQ_WtruYOip0793682541e"
@@ -45,7 +62,7 @@ encrypt_table = bytes.maketrans(decrypted, encrypted)
 decrypt_table = bytes.maketrans(encrypted, decrypted)
 
 
-
+#Creating Settings File
 
 #reading notes
 def noteprocess():
@@ -220,6 +237,42 @@ def sign_up():
         errorr = Label(text = "Account created. ", anchor='center', bg = "gray10", fg = "dodger blue")
         errorr.place(x= 250, y = 290, anchor = 'center')
 
+def autosave1():
+	global autosave
+	global darkmode
+	with open("assets/settings.scn") as settingsfile:
+		alllines1 = (settingsfile.readlines())
+		darkmode = str(alllines1[1].strip())
+	with open("assets/settings.scn") as settingsfile:
+		alllines = (settingsfile.readlines())
+		autosave = str(alllines[0].strip())
+	if autosave == "True":
+		autosavebutton.configure(image = boxunchecked)
+		autosave = "False"
+	elif autosave == "False":
+		autosavebutton.configure(image = boxchecked)
+		autosave = "True"
+	else:
+		print("urmum")
+	createsettingsfile()
+
+def createsettingsfile():
+
+	try:
+		with open("assets/settings.scn", "w") as sosage:
+			sosage.write(autosave)
+			sosage.write("\n")
+			sosage.write(darkmode)
+	except:
+		traceback.print_exc()
+		with open("assets/settings.scn", "x") as temp:
+			pass
+		with open("assets/settings.scn", "w") as idekanymore:
+			idekanymore.write("False")
+			idekanymore.write("\n")
+			idekanymore.write("True")
+
+
 def opensettings():
 
     inputbar.destroy()
@@ -233,12 +286,56 @@ def opensettings():
     signup.destroy()
     settingsbutton.destroy()
 
+    global PaschaHuevo
     global backbutton
-    backbutton = Button(text = "Back", command = mainmenu, anchor='center', bg = "gray10", activebackground= "gray10", borderwidth=0, fg = "snow")
-    backbutton.place(x= 20, y = 10, anchor = 'center')
+    backbutton = Button(text="Back", command = mainmenu, anchor='center', bg = "gray10", activebackground= "gray10", borderwidth=0, fg = "snow")
+    backbutton.place(x= 20, y = 385, anchor = 'center')
+    def checksautosavesetting():
+        global PaschaHuevo
+        try:
+            with open("assets/settings.scn") as settingsfile:
+                notes = (settingsfile.readlines())
+                line1 = str(notes[0])
+                print("line1 is '" + line1 + "'")
+            if line1.strip() == "True":
+                global PaschaHuevo
+                print("print this if ur bad")
+                PaschaHuevo = boxchecked
+            elif line1.strip() == "False":
+                print("danil is best")
+                PaschaHuevo = boxunchecked
+            else:
+            	print("line1 is '" + line1 + "'")
+        except:
+            open("assets/settings.scn", "x")
+            with open("assets/settings.scn", "w") as idekanymore:
+                idekanymore.write("False")
+                idekanymore.write("\n")
+                idekanymore.write("True")
+            checksautosavesetting()
+    checksautosavesetting()
+    print(PaschaHuevo)
+    
+    global autosavebutton
+    autosavebutton = Button(image = PaschaHuevo, command = autosave1, anchor='center', bg = "gray10", activebackground= "gray10", borderwidth=0, fg = "snow")
+    autosavebutton.place(x= 100, y = 115, anchor = 'center')
+
+    global autosavelabel
+    autosavelabel = Label(text = "- Autosave", bg = "gray10", fg = "snow", anchor = 'center', font = ("Ariel", 15))
+    autosavelabel.place(x = 120, y = 100)
+
+    #10.4 width, 10y
 
 
 def mainmenu():
+
+    try:
+        autosavelabel.destroy()
+        autosavebutton.destroy()
+        backbutton.destroy()
+        settings.destroy()
+    except:
+        pass
     #reads logins from text file in assets
     global logins
     
@@ -251,7 +348,7 @@ def mainmenu():
             except:
                 isempty = True
             
-            
+
     global inputbar
     global inputbar2
     global logolock
@@ -279,6 +376,7 @@ def mainmenu():
     ul2.place(x = 40, y = 200, anchor = 'center')
 
 
+
     #Input Bar
 
     global mess
@@ -301,8 +399,8 @@ def mainmenu():
 
     #settings button
     global settingsbutton
-    settingsbutton = Button(text = "Settings", command = opensettings, anchor='center', bg = "gray10", activebackground= "gray10", borderwidth=0, fg = "snow")
-    settingsbutton.place(x= 475, y = 385, anchor = 'center')
+    settingsbutton = Button(image = settings_icon, command = opensettings, anchor='center', bg = "gray10", activebackground= "gray10", borderwidth=0, fg = "snow")
+    settingsbutton.place(x= 485, y = 385, anchor = 'center')
 
 mainmenu()
 
